15bf47fcad78ac8ac17d2f8e67b2d9e3f8b071bf
usaqlain01 <usaqlain01@gmail.com>
2017-09-13 01:27:08 +0000

    adding four env variables for testing

--- drush/spark.aliases.drushrc.php
+++ drush/spark.aliases.drushrc.php
@@ -1,31 +1,51 @@
 <?php
-//  'remote-host' => '45.55.61.100',
-$common_live = array(
-  'remote-host' => '165.227.190.16',
+
+$prod_live = array(
+  'remote-host' => '45.55.61.100',
+  'remote-user' => 'roboking',
+);
+
+$qa_live = array(
+  'remote-host' => '45.55.187.23',
   'remote-user' => 'roboking',
 );
 
 $paths = array(
-  'local-root' => '/Users/clobby/dev/devdesktop/test/ccn',
+  'local-root' => '/Users/clobby/dev/devdesktop',
   'server-root' => '/var/www/spark',
 );
 
-$aliases['loc'] = array(
+$aliases['dev'] = array(
   'uri' => 'ccn.dd:8083',
-  'root' => $paths['local-root'].'/web',
+  'root' => $paths['local-root'].'/ccn/web',
   'path-aliases' => array(
-    '%dump-dir' => $paths['local-root'].'/drush/drush_dump',
-    '%files' => $paths['local-root'].'/web/sites/default/files',
+    '%dump-dir' => $paths['local-root'].'/ccn/drush/drush_dump',
+    '%files' => $paths['local-root'].'/ccn/web/sites/default/files',
   ),
   'command-specific' => array (
     'sql-sync' => array (
-      'target-dump' => $paths['local-root'].'/drush/drush_dump/example.sql',
+      'target-dump' => $paths['local-root'].'/ccn/drush/drush_dump/example.sql',
       'sanitize' => FALSE,
     ),
   ),
 );
 
-$aliases['dev'] = array(
+$aliases['test'] = array(
+  'uri' => 'ccntest.dd:8083',
+  'root' => $paths['local-root'].'/test/ccn/web',
+  'path-aliases' => array(
+    '%dump-dir' => $paths['local-root'].'/test/ccn/drush/drush_dump',
+    '%files' => $paths['local-root'].'/test/ccn/web/sites/default/files',
+  ),
+  'command-specific' => array (
+    'sql-sync' => array (
+      'target-dump' => $paths['local-root'].'/test/ccn/drush/drush_dump/example.sql',
+      'sanitize' => FALSE,
+    ),
+  ),
+);
+
+$aliases['stage'] = array(
   'uri' => 'default',
   'root' => $paths['server-root'].'/web',
   'path-aliases' => array(
@@ -40,4 +60,5 @@ $aliases['dev'] = array(
   ),
 );
 
-$aliases['stage'] = $aliases['dev'] + $common_live;
+$aliases['prod'] = $aliases['stage'] + $prod_live;
+$aliases['qa'] = $aliases['stage'] + $qa_live;
