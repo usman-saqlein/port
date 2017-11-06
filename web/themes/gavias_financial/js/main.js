c4ce7d8dac29bd300f68d659fc0c0f67f8b8af01
usaqlain01 <usaqlain01@gmail.com>
2017-11-06 18:10:18 +0000

    Adding JS Scroll

--- web/themes/gavias_financial/js/main.js
+++ web/themes/gavias_financial/js/main.js
@@ -435,17 +435,55 @@ $(window).on("debouncedresize", function(event) {
 });
 
 $(document).ready(function(){
-    var offset = -($(window).height() / 2) + 80; 
-    $(window).scroll(function () {
-        $('.service-timeline .entry-timeline').each(function(){
-            var top_menu_offset = $(this).offset();
-           if ($(window).scrollTop() > top_menu_offset.top + offset) {
-              $(this).addClass('active');
-            } else {
-              $(this).removeClass('active');
+  var offset = -($(window).height() / 2) + 80; 
+  $(window).scroll(function () {
+    $('.service-timeline .entry-timeline').each(function(){
+        var top_menu_offset = $(this).offset();
+       if ($(window).scrollTop() > top_menu_offset.top + offset) {
+          $(this).addClass('active');
+        } else {
+          $(this).removeClass('active');
+      }
+    });  
+  });
+
+  // Select all links with hashes
+    $('a[href*="#"]')
+      // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')  
+      .click(function(event) {
+        // On-page links  
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+          && 
+          location.hostname == this.hostname
+        ) { 
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000, function() {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              };
+            });
           }
-        });  
-    });
-  });  
+        }
+      });
+  
+});  
 
 })(jQuery);
